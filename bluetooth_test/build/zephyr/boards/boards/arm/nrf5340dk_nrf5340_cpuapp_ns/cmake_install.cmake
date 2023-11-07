cd463474d55a16ae096edbb05e91f7bf1b0b4a7a
Sauli <sauli.savinainen@gmail.com>
2023-11-07 11:45:54 +0200

    gitignore

--- bluetooth_test/build/zephyr/boards/boards/arm/nrf5340dk_nrf5340_cpuapp_ns/cmake_install.cmake
+++ bluetooth_test/build/zephyr/boards/boards/arm/nrf5340dk_nrf5340_cpuapp_ns/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/ncs/v2.5.0/zephyr/boards
+# Install script for directory: C:/ncs/v2.5.0/zephyr/boards/arm/nrf5340dk_nrf5340
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,10 +35,5 @@ endif()
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/ncs/toolchains/c57af46cb7/opt/zephyr-sdk/arm-zephyr-eabi/bin/arm-zephyr-eabi-objdump.exe")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("C:/Users/Sauli/Desktop/pylsav22/bluetooth_test/build/zephyr/boards/boards/arm/nrf5340dk_nrf5340_cpuapp_ns/cmake_install.cmake")
 endif()
 
