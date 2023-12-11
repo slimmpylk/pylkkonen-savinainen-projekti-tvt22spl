693c5648229f6d1b605bce6e65b09b1733a974ea
Sauli <sauli.savinainen@gmail.com>
2023-12-11 12:21:50 +0200

    Update main.c

--- viikko6/src/main.c
+++ viikko6/src/main.c
@@ -29,7 +29,7 @@
 #define USER_BUTTON_3           DK_BTN3_MSK
 #define USER_BUTTON_4           DK_BTN4_MSK
 
-#define DEBUG 1  // 0 = changes direction when button 3 is pressed
+#define DEBUG 0  // 0 = changes direction when button 3 is pressed
                  // 1 = fake 100 measurements done to each 6 directions when 3 pressed.
 static int direction = -1;	// 0 = x direction high
 							// 1 = x directon low	
@@ -90,7 +90,6 @@ static void button_changed(uint32_t button_state, uint32_t has_changed)
 
 		struct Measurement m = readADCValue();
 		printk("x = %d,  y = %d,  z = %d\n",m.x,m.y,m.z);
-		#endif
 	}		
 
 	if ((has_changed & USER_BUTTON_4) && (button_state & USER_BUTTON_4)) 
